b02f07c917283c257c89c336572f2f59ab18fcb7
Wilson <developer@wilsonso.com>
2020-09-22 00:20:43 -0700

    add hostname to backend

--- backend.tf
+++ backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "remote" {
+    hostname     = "app.terraform.io"
     organization = "WilsonSo"
 
     workspaces {
@@ -15,7 +16,9 @@ provider "aws" {
 data "terraform_remote_state" "terraform-cloud-tier1" {
   backend = "remote"
   config = {
+    hostname     = "app.terraform.io"
     organization = "WilsonSo"
+
     workspaces = {
       name = "terraform-cloud-tier1"
     }
