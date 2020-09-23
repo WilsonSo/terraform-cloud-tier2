679d560fa89b2eaf93ff8241f7f36605679bb2d3
Wilson <developer@wilsonso.com>
2020-09-22 20:56:42 -0700

    add additional subnets

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_subnet" "infra_internal" {
     Name = "infra_internal"
   }
 }
+
+resource "aws_subnet" "infra_external" {
+  vpc_id = data.terraform_remote_state.terraform-cloud-tier1.outputs.vpc_infra.id
+  cidr_block = "10.0.100.0/24"
+
+  tags = {
+    Name = "infra_external"
+  }
+}
