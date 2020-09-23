6c3958becb020e9e15ee5d8c994aa575fed49102
Wilson <developer@wilsonso.com>
2020-09-22 20:44:04 -0700

    add subnet output

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_subnet" "subnet1" {
+resource "aws_subnet" "infra_internal" {
   vpc_id = data.terraform_remote_state.terraform-cloud-tier1.outputs.vpc_infra.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "subnet1"
+    Name = "infra_internal"
   }
 }
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "infra_internal_subnet_id" {
+  value = "${aws_subnet.infra_internal.id}"
+}
