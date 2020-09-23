281684b44c927639334a7741c31e797f459bff71
Wilson <developer@wilsonso.com>
2020-09-22 20:56:42 -0700

    add additional subnets

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "infra_internal_subnet_id" {
   value = "${aws_subnet.infra_internal.id}"
 }
+
+output "infra_external_subnet_id" {
+  value = "${aws_subnet.infra_external.id}"
+}
+
+output "vpc_infra_id" {
+  value = data.terraform_remote_state.terraform-cloud-tier1.outputs.vpc_infra.id
+}
