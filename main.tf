4dec590bcc1e2b3038921ac0cabb96bdf023a1fd
Wilson <developer@wilsonso.com>
2020-09-22 18:51:21 -0700

    fix cidr block

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "subnet1" {
   vpc_id = data.terraform_remote_state.terraform-cloud-tier1.outputs.vpc_infra.id
-  cidr_block = "10.10.1.0/24"
+  cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = "subnet1"
